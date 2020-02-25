e684d9e386d762e3f11b6b4893dba75e3c8b1dba
Zakia <chabanezakia@gmail.com>
2020-02-25 17:35:27 +0100

    ajout fichier grille

---
+++ grille.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+class Grille:
+    def __init__ (self):
+	return 
+
+    def grille_initale (self):
+        plateau_de_jeu = [
+                         1, 2, 3,
+                         4, 5, 6,
+                         7, 8, 9,
+                                   ]
+                              
