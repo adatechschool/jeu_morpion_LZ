0ef39b59babad7d95ecc667e4af02316be29c58d
lydiasan <lyd.sanaa@gmail.com>
2020-02-26 17:41:01 +0100

    creation du fichier evenements.py - fichier à l'état de brouillon.

---
+++ evenements.py
@@ -0,0 +1,20 @@
+import grille.py
+
+# combinaisons de victoire possibles
+victoire = [[0,4,8], [2,4,6]
+            [0,3,6], [1,4,7], [2,5,8],
+            [0,1,2], [3,4,5], [6,7,8]
+            ]
+
+# définir case
+
+# définir cases_vides
+
+# match nul = aucune case vide et aucune victoire
+if case != cases_vides && cases q!= victoire
+    match_nul = true
+print "match nul"
+
+for x in range(len(plateau_de_jeu)):
+        for y in range(len(victoire[1])):
+            if plateau_de_jeu[x] == victoire[1][y]
